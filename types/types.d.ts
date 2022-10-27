e78714049f3d757131cb9fc008f8467ea208c75a
Allan Jardine <allan@datatables.net>
2022-10-27 15:12:07 +0000

    Update typing information to latest typing integration for DataTables

---
+++ types/types.d.ts
@@ -0,0 +1,21 @@
+
+// Dist-DataTables-Bootstrap5 integration with Bootstrap5 exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-Bootstrap5 integration with Bootstrap5 exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-Bootstrap5 integration with Bootstrap5 exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
