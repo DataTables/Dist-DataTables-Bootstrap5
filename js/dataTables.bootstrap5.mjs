29d58ea420ba19aed184c166aa792430571f1831
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-01-20 17:21:32 +0000

    93a604c3276e8a2a8cdb26e6bacdfdf37dc37f02 Fix: Accessability improvements with additional ARIA attributes for paging control
    https://datatables.net/forums/discussion/75174
    
    Sync to source repo @93a604c3276e8a2a8cdb26e6bacdfdf37dc37f02

--- js/dataTables.bootstrap5.mjs
+++ js/dataTables.bootstrap5.mjs
@@ -101,6 +101,8 @@ DataTable.ext.renderer.pageButton.bootstrap = function ( settings, host, idx, bu
 				}
 
 				if ( btnDisplay ) {
+					var disabled = btnClass.indexOf('disabled') !== -1;
+
 					node = $('<li>', {
 							'class': classes.sPageButton+' '+btnClass,
 							'id': idx === 0 && typeof button === 'string' ?
@@ -108,8 +110,9 @@ DataTable.ext.renderer.pageButton.bootstrap = function ( settings, host, idx, bu
 								null
 						} )
 						.append( $('<a>', {
-								'href': btnClass.indexOf('disabled') === -1 ? '#' : null,
+								'href': disabled ? null : '#',
 								'aria-controls': settings.sTableId,
+								'aria-disabled': disabled ? 'true' : null,
 								'aria-label': aria[ button ],
 								'aria-role': 'link',
 								'aria-current': btnClass === 'active' ? 'page' : null,
